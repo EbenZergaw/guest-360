892cf5c3a110b7154ff3b84f3c30f829f70a65ec
Omer Ahmed <omeryosif.ai@gmail.com>
2024-10-20 14:04:36 -0400

    updated code to match sample schema

--- myapi/api/serializers.py
+++ myapi/api/serializers.py
@@ -7,85 +7,50 @@ class BookingSerializer(serializers.ModelSerializer):
         fields = ['city', 'hotel', 'check_in_date', 'check_out_date', 'number_of_rooms', 'number_of_guests']
 
 class RoomPreferencesSerializer(serializers.ModelSerializer):
-    location = serializers.SerializerMethodField()
     type = serializers.CharField(source='room_type')
+    location = serializers.ListField(child=serializers.CharField(), source='room_locations')
     temperature = serializers.IntegerField(source='room_temperature')
 
     class Meta:
         model = Preferences
         fields = ['type', 'location', 'temperature']
 
-    def get_location(self, obj):
-        return [location.location for location in obj.room_locations.all()]
-
 class FoodPreferencesSerializer(serializers.ModelSerializer):
-    favorites = serializers.SerializerMethodField()
-    dietary_restrictions = serializers.SerializerMethodField()
+    favorites = serializers.ListField(child=serializers.CharField())
+    dietary_restrictions = serializers.ListField(child=serializers.CharField())
 
     class Meta:
         model = FoodPreference
         fields = ['favorites', 'dietary_restrictions']
 
-    def get_favorites(self, obj):
-        return [food.name for food in obj.favorites.all()]
-
-    def get_dietary_restrictions(self, obj):
-        return [restriction.name for restriction in obj.dietary_restrictions.all()]
-
 class BeveragePreferencesSerializer(serializers.ModelSerializer):
-    non_alcoholic = serializers.SerializerMethodField()
-    alcoholic = serializers.SerializerMethodField()
+    non_alcoholic = serializers.ListField(child=serializers.CharField())
+    alcoholic = serializers.ListField(child=serializers.CharField())
 
     class Meta:
         model = BeveragePreference
         fields = ['non_alcoholic', 'alcoholic']
 
-    def get_non_alcoholic(self, obj):
-        return [beverage.name for beverage in obj.non_alcoholic.all()]
-
-    def get_alcoholic(self, obj):
-        return [beverage.name for beverage in obj.alcoholic.all()]
-
 class PreferencesSerializer(serializers.ModelSerializer):
-    room = serializers.SerializerMethodField()
-    pillow_type = serializers.SerializerMethodField()
-    amenities = serializers.SerializerMethodField()
-    food_preferences = FoodPreferencesSerializer(required=False)
-    beverages = BeveragePreferencesSerializer(source='beverage_preferences', required=False)
+    room = RoomPreferencesSerializer()
+    pillow_type = serializers.ListField(child=serializers.CharField())
+    amenities = serializers.ListField(child=serializers.CharField())
+    food_preferences = FoodPreferencesSerializer()
+    beverages = BeveragePreferencesSerializer()
 
     class Meta:
         model = Preferences
         fields = ['accessible', 'bed_type', 'room', 'pillow_type', 'amenities', 'food_preferences', 'beverages']
 
-    def get_room(self, obj):
-        return {
-            'type': obj.room_type,
-            'location': [location.location for location in obj.room_locations.all()],
-            'temperature': obj.room_temperature
-        }
-
-    def get_pillow_type(self, obj):
-        return [pillow.type for pillow in obj.pillow_types.all()]
-
-    def get_amenities(self, obj):
-        return [amenity.name for amenity in obj.amenities.all()]
-
 class GuestSerializer(serializers.ModelSerializer):
-    upcoming_bookings = BookingSerializer(many=True, read_only=True)
-    past_bookings = BookingSerializer(many=True, read_only=True)
-    preferences = PreferencesSerializer(required=False)
+    upcoming_bookings = BookingSerializer(many=True)
+    past_bookings = BookingSerializer(many=True)
+    preferences = PreferencesSerializer()
 
     class Meta:
         model = Guest
         fields = ['first_name', 'last_name', 'birthday', 'gender', 'bonvoy_id', 'email', 'phone_number', 'upcoming_bookings', 'past_bookings', 'preferences']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['upcoming_bookings'] = BookingSerializer(instance.bookings.filter(is_past=False), many=True).data
-        representation['past_bookings'] = BookingSerializer(instance.bookings.filter(is_past=True), many=True).data
-        if hasattr(instance, 'preferences'):
-            representation['preferences'] = PreferencesSerializer(instance.preferences).data
-        return representation
+        read_only_fields = ['bonvoy_id']
 
     def create(self, validated_data):
         upcoming_bookings_data = validated_data.pop('upcoming_bookings', [])
@@ -101,8 +66,7 @@ class GuestSerializer(serializers.ModelSerializer):
             Booking.objects.create(guest=guest, is_past=True, **booking_data)
 
         if preferences_data:
-            preferences = self.create_preferences(guest, preferences_data)
-            # Here you could do something with the returned preferences if needed
+            self.create_preferences(guest, preferences_data)
 
         return guest
 
@@ -139,84 +103,3 @@ class GuestSerializer(serializers.ModelSerializer):
             NonAlcoholicBeverage.objects.create(beverage_preference=beverage_pref, name=non_alcoholic)
         for alcoholic in beverage_preferences_data.get('alcoholic', []):
             AlcoholicBeverage.objects.create(beverage_preference=beverage_pref, name=alcoholic)
-
-        return preferences  # Return the created Preferences instance
-
-    def update(self, instance, validated_data):
-        # Update basic guest information
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.birthday = validated_data.get('birthday', instance.birthday)
-        instance.gender = validated_data.get('gender', instance.gender)
-        instance.email = validated_data.get('email', instance.email)
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-        instance.save()
-
-        # Update bookings
-        upcoming_bookings_data = validated_data.get('upcoming_bookings', [])
-        past_bookings_data = validated_data.get('past_bookings', [])
-
-        instance.bookings.filter(is_past=False).delete()
-        instance.bookings.filter(is_past=True).delete()
-
-        for booking_data in upcoming_bookings_data:
-            Booking.objects.create(guest=instance, is_past=False, **booking_data)
-        
-        for booking_data in past_bookings_data:
-            Booking.objects.create(guest=instance, is_past=True, **booking_data)
-
-        # Update preferences
-        preferences_data = validated_data.get('preferences')
-        if preferences_data:
-            preferences, created = Preferences.objects.get_or_create(guest=instance)
-            
-            preferences.accessible = preferences_data.get('accessible', preferences.accessible)
-            preferences.bed_type = preferences_data.get('bed_type', preferences.bed_type)
-            room_data = preferences_data.get('room', {})
-            preferences.room_type = room_data.get('type', preferences.room_type)
-            preferences.room_temperature = room_data.get('temperature', preferences.room_temperature)
-            preferences.save()
-
-            # Update room locations
-            preferences.room_locations.all().delete()
-            for location in room_data.get('location', []):
-                RoomLocation.objects.create(preferences=preferences, location=location)
-
-            # Update pillow types
-            preferences.pillow_types.all().delete()
-            for pillow_type in preferences_data.get('pillow_type', []):
-                PillowType.objects.create(preferences=preferences, type=pillow_type)
-
-            # Update amenities
-            preferences.amenities.all().delete()
-            for amenity in preferences_data.get('amenities', []):
-                Amenity.objects.create(preferences=preferences, name=amenity)
-
-            # Update food preferences
-            food_preferences_data = preferences_data.get('food_preferences', {})
-            food_pref, created = FoodPreference.objects.get_or_create(preferences=preferences)
-            
-            food_pref.favorites.all().delete()
-            for favorite in food_preferences_data.get('favorites', []):
-                FavoriteFood.objects.create(food_preference=food_pref, name=favorite)
-
-            food_pref.dietary_restrictions.all().delete()
-            for restriction in food_preferences_data.get('dietary_restrictions', []):
-                DietaryRestriction.objects.create(food_preference=food_pref, name=restriction)
-
-            # Update beverage preferences
-            beverage_preferences_data = preferences_data.get('beverages', {})
-            beverage_pref, created = BeveragePreference.objects.get_or_create(preferences=preferences)
-            
-            beverage_pref.non_alcoholic.all().delete()
-            for non_alcoholic in beverage_preferences_data.get('non_alcoholic', []):
-                NonAlcoholicBeverage.objects.create(beverage_preference=beverage_pref, name=non_alcoholic)
-
-            beverage_pref.alcoholic.all().delete()
-            for alcoholic in beverage_preferences_data.get('alcoholic', []):
-                AlcoholicBeverage.objects.create(beverage_preference=beverage_pref, name=alcoholic)
-
-        return instance
-
-class GuestResponseSerializer(serializers.Serializer):
-    guest = GuestSerializer()
--- myapi/api/views.py
+++ myapi/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Guest
-from .serializers import GuestResponseSerializer, GuestSerializer
+from .serializers import GuestSerializer
 
 class GuestListView(APIView):
     def get(self, request):
@@ -25,22 +25,21 @@ class GuestListView(APIView):
         serializer = GuestSerializer(data=request.data)
         if serializer.is_valid():
             guest = serializer.save()
-            response_serializer = GuestResponseSerializer({'guest': guest})
-            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(GuestSerializer(guest).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class GuestDetailView(APIView):
     def get(self, request, bonvoy_id):
-        guest = get_object_or_404(Guest, bonvoy_id=bonvoy_id)
-        serializer = GuestResponseSerializer({'guest': guest})
-        return Response(serializer.data)
-
-class GuestByBonvoyIdView(APIView):
-    def get(self, request, bonvoy_id):
-        try:
-            guest = Guest.objects.get(bonvoy_id=bonvoy_id)
-        except Guest.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
+        guest = get_object_or_404(Guest.objects.prefetch_related(
+            'bookings',
+            'preferences',
+            'preferences__room_locations',
+            'preferences__pillow_types',
+            'preferences__amenities',
+            'preferences__food_preferences__favorites',
+            'preferences__food_preferences__dietary_restrictions',
+            'preferences__beverage_preferences__non_alcoholic',
+            'preferences__beverage_preferences__alcoholic'
+        ), bonvoy_id=bonvoy_id)
         serializer = GuestSerializer(guest)
         return Response(serializer.data)
