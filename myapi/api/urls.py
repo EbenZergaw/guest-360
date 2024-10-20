0de7fad799975fc7d19c36bedad2d46e9549bb4a
Omer Ahmed <omeryosif.ai@gmail.com>
2024-10-20 14:13:54 -0400

    Corrected app name in INSTALLED_APPS and created initial migrations

--- myapi/api/urls.py
+++ myapi/api/urls.py
@@ -6,5 +6,5 @@ from .views import GuestListView, GuestDetailView
 
 urlpatterns = [
     path('guests/', GuestListView.as_view(), name='guest-list'),
-    path('guests/<uuid:bonvoy_id>/', GuestDetailView.as_view(), name='guest-detail'),
+    path('guests/<str:bonvoy_id>/', GuestDetailView.as_view(), name='guest-detail'),
 ]
