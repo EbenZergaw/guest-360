c87da6809a245732092f8dc19981d72406f2a4f2
omerahmed05 <omera@vt.edu>
2024-10-19 22:10:01 -0400

    Added api app to INSTALLED APPS

--- myapi/urls.py
+++ myapi/urls.py
@@ -9,10 +9,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
 def home(request):
     return HttpResponse("Welcome to Guest360 API")
 
